f0ee4c014c20635fbf13c6d3b26a64b52212ee50
Morgan Houston <morgan.houston@okstate.edu>
2020-10-07 17:36:23 -0500

    commit

---
+++ CC Unit 2 Challenge 2-Houston-Morgan/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour {
+    public GameObject dogPrefab;
+    private float resetTime = 0.0f;
+
+    // Update is called once per frame
+    void Update() {
+        resetTime -= Time.deltaTime;
+        // On spacebar press, send dog
+        if (resetTime <= 0) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                resetTime = 2.5f;
+            }
+        }
+        
+    }
+}
---
+++ CC Unit 2 Challenge 2-Houston-Morgan/Assets/Challenge 2/Scripts/ScoreManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static int ballsCaught = 0;
+    public static int ballsMissed = 0;
+    Text scoreTxt;
+    // Start is called before the first frame update
+    void Start() {
+        scoreTxt = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        scoreTxt.text = "Balls Caught: " + ballsCaught + "\nBalls Missed: " + ballsMissed;
+    }
+
+    public static void ballCaught() {
+        ballsCaught += 1;
+    }
+
+    public static void ballMiss()
+    {
+        ballsMissed += 1;
+    }
+}
